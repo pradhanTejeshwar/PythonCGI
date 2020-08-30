79c4846521a6dbd0b4c3fc15d3d9ce92d2961b3c
Laboni-31 <56451703+Laboni-31@users.noreply.github.com>
2020-08-30 17:53:07 +0700

    Designing Search bar using  PythonCGI

---
+++ search.py
@@ -0,0 +1,26 @@
+#!C:\Python\Python3.7.4\python.exe
+print("Content-type:text/html\r\n\r\n")
+
+
+print('<!DOCTYPE html>')
+print('<html lang="en">')
+print('<head>')
+print('	<meta charset="UTF-8"> ')
+print('	<meta name="viewport" content="width=device-width, initial-scale=1.0">')
+print('	<title>Transparent Search Box</title>')
+print('	<link rel="stylesheet" href="styles.css">')
+print('	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">')
+
+
+print('</head>')
+print('<body>')
+print('<div class="wrapper">')
+print('  <div class="search_box">')
+print('<form action = "demo.py" method = "" >')
+print('      <input type="text" placeholder="what are you looking for?">')
+print('<button type="submit"><i class="fas fa-search"></i></button>')
+print('</form>')
+print('  </div>')
+print('</div>')
+print('</body>')
+print('</html>')
